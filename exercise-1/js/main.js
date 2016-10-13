9376bd1c2f736db2005646b16f3cfb2871cc3530
MyungJin Eun <eunmyun@uw.edu>
2016-10-13 10:20:49 -0700

    class exercises

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -6,19 +6,19 @@ var changeText;
 // You'll have to wait for you page to load to assign events to the elements created in your index.html File
 $(function(){
   // Select elements with class "italics" and set their css "font-style", to "italic"
-
+  $('.italics').css('font-style', 'italic');
 
   // Select all svgs and change their attr "height" to 200
-
+  $('svg').css('height', '200');
 
   // Select your first svg and change it's width attribute to 10
-
+  $('svg').filter(":first").css('width', '10');
 
   // Select even rows in the table and set their css "background" to "lightgray"
-
+  $('tr:even').css('background', 'lightgray');
 
   // Select the input and set the attribute "placeholder" to "start typing"
-
+  $('input').attr('placeholder', 'start typing');
 
   // Set the input val to "hello"
 
